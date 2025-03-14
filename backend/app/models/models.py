96862acd4b4630ded6e68964f98ddd3f283a59f2
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2025-03-14 16:37:43 +0100

    Started backend directiory

---
+++ backend/app/models/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    name = Column(String)
+    surname = Column(String)
+    hashed_password = Column(String)
